17aef7b4c85655ac59e6f0eacc26bb036111e481
bhimanshukalra <bhimanshukalra@gmail.com>
2026-06-12 23:20:03 +0530

    Add NativeWind to mobile app

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import "./global.css";
+
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { AppNavigator } from "./src/navigation/AppNavigator";
